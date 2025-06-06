3f8f70cda23862ddd6448c7b758be088347841c1
Deswandy <162786961+Deswandy@users.noreply.github.com>
2025-06-06 18:06:35 +0700

    Update plotData.m
    removed fft

--- plotData.m
+++ plotData.m
@@ -4,10 +4,10 @@ function [price, date] = plotData(filename, fs, titlePrefix)
     date = datetime(data.Price,'InputFormat','yyyy-MM-dd');
     xDays = 0:1/fs:length(price)-1;
 
-    Nfft = 2048;
-    Fs = fft(price, Nfft);
-    AFs = abs(Fs);
-    f = fs/Nfft*(0:Nfft/2 - 1);
+    % Nfft = 2048;
+    % Fs = fft(price, Nfft);
+    % AFs = abs(Fs);
+    % f = fs/Nfft*(0:Nfft/2 - 1);
 
     % Subplots: Time (date), Time (index), Frequency
     %figure('Name', char([titlePrefix ' - Raw Data']), 'NumberTitle', 'off');
@@ -19,13 +19,13 @@ function [price, date] = plotData(filename, fs, titlePrefix)
 %    title([titlePrefix ' - Time Domain (Date)']);
 %    xlabel('Date'); ylabel('Price'); grid on;
 
-    subplot(2,1,1);
+    % subplot(2,1,1);
     plot(xDays, price);
     title([titlePrefix ' - Time Domain (Index)']);
     xlabel('Days'); ylabel('Price'); grid on;
 
-    subplot(2,1,2);
-    plot(f, AFs(1:Nfft/2));
-    title([titlePrefix ' - Frequency Domain']);
-    xlabel('Frequency (cycles/day)'); ylabel('Magnitude'); grid on;
+    % subplot(2,1,2);
+    % plot(f, AFs(1:Nfft/2));
+    % title([titlePrefix ' - Frequency Domain']);
+    % xlabel('Frequency (cycles/day)'); ylabel('Magnitude'); grid on;
 end
